cb001abc39434ca630e0a4226367f00ac288752d
dan90ned <56802812+dan90ned@users.noreply.github.com>
2019-12-05 21:18:34 +0200

    a

--- BlackJack.py
+++ BlackJack.py
@@ -2,8 +2,10 @@
 The game will not end by itself whe the credit is 0 for one of them
 Have fun!"""
 
-
 import random
+import sys
+import os
+import time
 
 # create a deck dictionary with key and values
 deck = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10,
@@ -79,7 +81,7 @@ def check_bj(hand):  # check BJ
     sum_hand = sum(deck[i] for i in hand)
 
     if sum_hand == 21:
-        print(f'Black Jack! {hand.name} wins!')
+        output_handle(f'Black Jack! {hand.name} wins!')
         play = False
     else:
         play = True
@@ -146,29 +148,29 @@ def check_over21(hand):
 def print_sum_cards(hand):  # print sum card w or w/o A in hand
     try:
         if check_a(hand)[0] == 2 and 'A' in list(hand):
-            print(f'{hand.name} Total: {check_cards(hand, True)[1]} / {check_cards(hand, True)[2]}')
+            output_handle(f'{hand.name} Total: {check_cards(hand, True)[1]} / {check_cards(hand, True)[2]}')
         else:
-            print(f'{hand.name} Total: {check_cards(hand, True)[1]}')
+            output_handle(f'{hand.name} Total: {check_cards(hand, True)[1]}')
     except ValueError:
-        print(f'{hand.name} Total: {check_cards(hand, True)[1]}')
+        output_handle(f'{hand.name} Total: {check_cards(hand, True)[1]}')
 
 
 def check_winner(player_hand, pc_hand):  # check the winner (to be payed later)
     global winner
     if check_cards(player_hand, True)[1] > check_cards(pc_hand, True)[1]:
         winner = 1
-        print(f'{player_hand.name} wins!')
+        output_handle(f'{player_hand.name} wins!')
     elif check_cards(player_hand, True)[1] < check_cards(pc_hand, True)[1]:
-        print(f'{pc_hand.name} wins!')
+        output_handle(f'{pc_hand.name} wins!')
         winner = 2
     elif check_cards(player_hand, True)[1] == check_cards(pc_hand, True)[1]:
-        print('Deuce!')
+        output_handle('Deuce!')
 
 
 def hit_stand_in():  # check if player wants to hit or to stand
     while True:
         try:
-            choose = input('Please choose: Hit(H) or Stand(S): '.strip()).upper()
+            choose = input_handle('Please choose: Hit(H) or Stand(S): ').strip().upper()
             if choose == 'H':
                 player1.next_deal()
                 player_hit = True
@@ -203,7 +205,7 @@ def check_total(hand):  # check cards total and return cases of game
 def check_play_again():  # check if player want to play again
     while True:
         try:
-            choose = input('Do you want to play again? (Y/N): '.strip()).upper()
+            choose = input_handle('Do you want to play again? (Y/N): ').strip().upper()
             if choose == 'Y':
                 play_again = True
                 break
@@ -225,10 +227,10 @@ def bet():  # bet func
     global bet_amount
     while True:
         try:
-            bet_amount = int(input('Please place your bet: '.strip()).upper())
+            bet_amount = int(input_handle('Please place your bet: ').strip().upper())
 
             if player1.balance >= bet_amount > 0:
-                pass
+                return bet_amount
             else:
                 bet()
             break
@@ -246,12 +248,17 @@ def pay_winner():  # pay winner func after the game is end
 
 
 def output_handle(printed_arg):
-    print(printed_arg)
+    sys.stdout.write(printed_arg + '\n')
+
+
+def input_handle(prompt):
+    sys.stdout.write(prompt + '\n')
+    return sys.stdin.readline()
 
 
 def print_first_hand():
-    return f'{player1.name} cards:   {player1} \n'\
-           f'{pc.name} cards: [ {list(pc)[0]},   --] \n'\
+    return f'{player1.name} cards:   {player1} \n' \
+           f'{pc.name} cards: [ {list(pc)[0]},   --] \n' \
            '\n'
 
 
@@ -261,24 +268,27 @@ def print_credit():  # print credit func
            f'{player1.name} credit: {player1.balance} \n' \
            f'{pc.name} credit: {pc.balance} \n' \
            '$$$$$$$$$$$$$$$$$$$$$ \n' \
-            '\n'
+           '\n'
 
 
-with open('cfg.txt') as f:
-    d = {}
-    for line in f:
-        k, v = line.split(':')
-        d[k] = int(v)
-k_list = [k for k in d]
+def clear_screen():
+    os.system('cls')
 
 
 if __name__ == '__main__':
+    with open('cfg.txt') as f:
+        d = {}
+        for line in f:
+            k, v = line.split(':')
+            d[k] = int(v)
+    k_list = [k for k in d]
+
     # set players
     player1 = Player(k_list[0], d[k_list[0]])
     pc = Player(k_list[1], d[k_list[1]])
 
     # execution
-    output_handle(print_credit())
+
     while True:  # game loop (play again func)
         if len(used_deck) >= 10:  # shuffle deck if card are less then 10
             pass
@@ -287,7 +297,8 @@ if __name__ == '__main__':
 
         # game start
         winner = 0
-
+        clear_screen()
+        output_handle(print_credit())
         bet()
 
         player1.f_deal()  # deal cards
@@ -307,10 +318,9 @@ if __name__ == '__main__':
         while check_total(player1) == 1:  # loop for hit/stand
             if hit_stand_in():
 
-                print('----------------------')
-                print(f'{player1.name} cards:   {player1}')
-                print(f'{pc.name} cards: [ {list(pc)[0]},   --]')
-                print()
+                output_handle(f'---------------------- \n'
+                              f'{player1.name} cards:   {player1}\n'
+                              f'{pc.name} cards: [ {list(pc)[0]},   --]\n')
 
                 print_sum_cards(player1)
 
@@ -319,7 +329,7 @@ if __name__ == '__main__':
 
                 elif check_total(player1) == 2:
                     winner = 2
-                    print('Bust! You lost!')
+                    output_handle('Bust! You lost!')
                     break
 
                 elif check_total(player1) == 3:
@@ -328,22 +338,23 @@ if __name__ == '__main__':
                 break
 
         if check_total(player1) in (1, 3) and check_bj(player1)[1]:  # computer loop
-            print('----------------------')
-            print(f'{pc.name} cards: {pc}')
+            output_handle(f'----------------------\n'
+                          f'{pc.name} cards: {pc}\n')
             check_bj(pc)
             print_sum_cards(pc)
             while check_a(pc)[1] < 17:
-                print('----------------------')
+                output_handle('----------------------')
+                time.sleep(2)
                 pc.next_deal()
-                print(f'{pc.name} cards: {pc}')
+                output_handle(f'{pc.name} cards: {pc}')
 
                 print_sum_cards(pc)
 
             else:
                 if check_a(pc)[1] > 21:
                     winner = 1
-                    print(f'{pc.name} Bust! \n'
-                          f'{player1.name} wins!')
+                    output_handle(f'{pc.name} Bust! \n'
+                                  f'{player1.name} wins!')
                 else:
                     print_sum_cards(player1)
                     check_winner(player1, pc)
@@ -353,7 +364,7 @@ if __name__ == '__main__':
         output_handle(print_credit())
 
         if check_play_again():
-            print('*****************************')
+            clear_screen()
             player1.clear_cards()
             pc.clear_cards()
         else:
--- Tests/test_bet.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from Projects import BlackJack
-
-player1 = 100
-
-
-class TestBet(unittest.TestCase):
-    def test_bet(self):
-        self.assertGreater(BlackJack.bet(), player1, 'alo')
